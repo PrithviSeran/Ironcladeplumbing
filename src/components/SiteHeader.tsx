ce590425f4ba0ed794bd37afa36538d0fe29fca0
prithviseran <prithisha33@gmail.com>
2026-05-16 16:17:18 -0400

    fix mobile

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -188,9 +188,7 @@ export function SiteHeader() {
                 d="M6.62 10.79a15.05 15.05 0 0 0 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V21c0 .55-.45 1-1 1C10.07 22 2 13.93 2 3c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"
               />
             </svg>
-            <span>
-              <strong>{PHONE_DISPLAY}</strong> FOR PLUMBING INQUIRIES
-            </span>
+            <span>{PHONE_DISPLAY}</span>
           </a>
           <button
             type="button"
